e56e73ce4396c803deb2e366006b46f52963f170
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2021-06-09 08:26:43 +0530

    # Image Uploading from Assets

--- margin_padding_box_constraints/lib/app_screens/home.dart
+++ margin_padding_box_constraints/lib/app_screens/home.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatelessWidget {
-
 //Container widget
   @override
   Widget build(BuildContext context) {
     return Center(
-        child : (
-        Container(
-     alignment:  Alignment.center,
-     color: Colors.blue,
-     child: Text("Nagalakshmi", textDirection: TextDirection.ltr,
-     style: TextStyle(
-       decoration: TextDecoration.none,
-       fontSize: 20.0,
-       fontFamily: 'Raleway',
-       fontWeight: FontWeight.w300,
-     ))
-   )
-        )
+      child: (
+      LoadImagefromAssets()
+      )
     );
   }
+}
 
+class LoadImagefromAssets extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+   AssetImage assetImage = AssetImage("images/photo.jpeg");
+   Image image = Image(image: assetImage);
+   return Container(child: image);
+  }
 
 }
