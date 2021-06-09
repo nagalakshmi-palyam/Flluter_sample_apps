a2056471997648900caa43530066ce3107d03e3a
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2021-06-09 09:16:35 +0530

    Alert Dialog added

--- margin_padding_box_constraints/lib/app_screens/home.dart
+++ margin_padding_box_constraints/lib/app_screens/home.dart
@@ -1,24 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
 //Container widget
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: (
-      LoadImagefromAssets()
-      )
+  Widget build(BuildContext context)=> Container(
+     color: Colors.white,
+     child:  Column(
+    children: <Widget>[
+      LoadImagefromAssets(),
+      RaiseButton()
+        ]
+    )
     );
-  }
+
 }
 
+//Loading Images from Assets
 class LoadImagefromAssets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
    AssetImage assetImage = AssetImage("images/photo.jpeg");
    Image image = Image(image: assetImage);
-   return Container(child: image);
+   return Container(
+       margin: EdgeInsets.only(top: 100.0),
+       child: image
+   );
   }
 
 }
+
+//Raised button
+class RaiseButton extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+     child: RaisedButton(
+       color: Colors.deepOrange,
+       child: Text("Button",
+       style: TextStyle(
+           fontSize: 20.0,
+         fontFamily: 'Raleway'
+       )),
+       elevation: 6.0,
+       onPressed: () => AlertDialogmethod(context)
+     )
+   );
+  }
+
+  //AlertDialog
+  void AlertDialogmethod(BuildContext context){
+    var alertDialog = AlertDialog(
+      title: Text("Button Clicked"),
+      content: Text("This example is quite better"),
+    );
+    showDialog(context: context, builder: (
+    BuildContext context) =>  alertDialog
+    );
+  }
+
+}
+
+
+
+
